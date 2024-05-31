58255c433cf854ba05b3d091ff2b0afa1724e477
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2024-05-31 18:51:04 +0200

    MVP

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,61 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
 type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
+  [key in keyof CloudflareBindings]: CloudflareBindings[key];
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/login", (c) => {
+  const formatedUrl = `${c.env.LOGIN_URL}`
+    .replace("CLIENTID", `${c.env.CLIENT_ID}`)
+    .replace("REDIRECT_URL", `${c.env.REDIRECT_URL}`);
+  return c.json({ loginUrl: formatedUrl });
+});
 
-export default app
+app.post("/authorize", async (c) => {
+  interface SuccessAuthorization {
+    meta: {
+      http_code: number;
+    };
+    response: {
+      access_token: string;
+    };
+  }
+
+  const body = await c.req.json();
+
+  if (!body) {
+    return c.status(500);
+  }
+
+  const { code } = body;
+
+  if (!code) {
+    console.error("Invalid parameters");
+    return c.status(500);
+  }
+
+  const formatedUrl = `${c.env.AUTHORIZE_URL}`
+    .replace("CLIENTID", `${c.env.CLIENT_ID}`)
+    .replace("CLIENTSECRET", `${c.env.CLIENT_SECRET}`)
+    .replace("REDIRECT_URL", `${c.env.REDIRECT_URL}`)
+    .replace("CODE", `${code}`);
+
+  return fetch(formatedUrl, {
+    headers: {
+      "User-Agent": `${c.env.APP_NAME} ${c.env.CLIENT_ID}`,
+    },
+  })
+    .then(async (res) => {
+      const json: SuccessAuthorization = await res.json();
+      console.log(json);
+      return c.json({ acess_token: `${json.response.access_token}` });
+    })
+    .catch((error) => {
+      console.error("Error during authorization:", error);
+      return c.status(500);
+    });
+});
+
+export default app;
