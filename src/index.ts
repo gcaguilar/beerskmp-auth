39d5d5457df15aa29ccb1702674340f9b21b9ae9
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2024-06-03 12:11:19 +0200

    Minor fixes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,32 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+app.get("/.well-known/assetlinks.json", (c) => {
+  type PermissionConfig = {
+    relation: string[];
+    target: {
+      namespace: string;
+      package_name: string;
+      sha256_cert_fingerprints: string[];
+    };
+  };
+
+  const permissions: PermissionConfig[] = [
+    {
+      relation: ["delegate_permission/common.handle_all_urls"],
+      target: {
+        namespace: "android_app",
+        package_name: "org.gcaguilar.kmmbeers",
+        sha256_cert_fingerprints: [
+          "8D:0A:32:E1:AE:45:DB:B4:BF:D5:6D:B5:4D:8C:DC:56:41:0F:F6:7A:CA:68:99:24:AB:FE:2B:21:5B:EE:EA:BF",
+        ],
+      },
+    },
+  ];
+
+  return c.json(permissions);
+});
+
 app.get("/login", (c) => {
   const formatedUrl = `${c.env.LOGIN_URL}`
     .replace("CLIENTID", `${c.env.CLIENT_ID}`)
@@ -50,7 +76,7 @@ app.post("/authorize", async (c) => {
     .then(async (res) => {
       const json: SuccessAuthorization = await res.json();
       console.log(json);
-      return c.json({ acess_token: `${json.response.access_token}` });
+      return c.json({ access_token: `${json.response.access_token}` });
     })
     .catch((error) => {
       console.error("Error during authorization:", error);
